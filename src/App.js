8cbb58be89c76d6bc1910a45545ee0f34b7efd89
Endertle <nethboy557@gmail.com>
2022-11-18 19:21:06 +0800

    404 route added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import CreatePost from "./components/CreatePost";
 import PostDetails from "./components/PostDetails";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route path="/createPost" element={<CreatePost />} />
           <Route path="/post/:id" element={<PostDetails />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </div>
     </div>
